61adccea48eeebc37d46fb168483aea1d8353a9d
Ryan Lopopolo <rjl@hyperbo.la>
2012-04-10 17:21:57 -0400

    Message passing server stubs
    Screw sockets and AMPQ. I'm just going to expose 3 restful apis on each
    node (1 for each node type). The master also gets a restful api

---
+++ lib/sr/server/collector.rb
@@ -0,0 +1,23 @@
+require "sr"
+
+require "sinatra/base"
+
+module Sr
+  module Collector
+    attr_accessor :collector_port
+
+    class Server < Sinatra::Base
+      POST "/new-job" do
+        # create reducer and add it to the pool of reducers in this node
+        Reducer.new(params[:job_id].to_i, eval(params[:combine_block]))
+      end
+    end
+
+    def self.start_server port
+      @collector_port = port # save this for later
+      Server.run! :port => port
+      # TODO: contact master and tell it a new collector is up
+    end
+  end
+end
+

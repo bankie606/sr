4dab7b4e4e311f4edda01508639fd0b8e9e9799d
Ryan Lopopolo <rjl@hyperbo.la>
2012-04-17 15:32:17 -0400

    Require everything in lib/sr

--- lib/sr.rb
+++ lib/sr.rb
@@ -2,3 +2,17 @@ module Sr
   VERSION = "0.0.1"
   UUID = begin `uuidgen`.strip rescue rand(10000) end
 end
+
+require "sr/collector"
+require "sr/fetcher"
+require "sr/master"
+require "sr/node"
+require "sr/util"
+require "sr/worker"
+
+require "sr/server/collector"
+require "sr/server/fetcher"
+require "sr/server/master"
+require "sr/server/message-types"
+require "sr/server/worker"
+

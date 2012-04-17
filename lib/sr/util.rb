eb6acc1645e951c2a070e4229e2800f4baf5bc1b
Ryan Lopopolo <rjl@hyperbo.la>
2012-04-17 16:57:54 -0400

    Add routine to compute local ip

--- lib/sr/util.rb
+++ lib/sr/util.rb
@@ -1,7 +1,9 @@
 require "sr"
 
+require "json"
 require "uri"
 require "net/http"
+require 'socket'
 
 module Sr
   class Util
@@ -9,11 +11,24 @@ module Sr
 
       # send a message over http and return the response body
       def send_message(destination, message_type, params)
-        req_uri = "#{destination}/#{message_type}?"
+        req_uri = "http://#{destination}/#{message_type}?"
         params.each_pair do |k,v|
           req_uri += "#{k}=#{v}&"
         end
-        Net::HTTP.get_response(URI.parse(req_uri)).read_body
+        JSON.parse(Net::HTTP.get_response(URI.parse(req_uri)).read_body)
+      end
+
+      # http://coderrr.wordpress.com/2008/05/28/get-your-local-ip-address/
+      def local_ip
+        # turn off reverse DNS resolution temporarily
+        orig, Socket.do_not_reverse_lookup = Socket.do_not_reverse_lookup, true
+
+        UDPSocket.open do |s|
+          s.connect '64.233.187.99', 1
+          s.addr.last
+        end
+      ensure
+        Socket.do_not_reverse_lookup = orig
       end
     end
   end

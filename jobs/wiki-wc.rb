5c5bae6d891253fd57d1fe7bb8aa904fa9591c36
Ryan Lopopolo <rjl@hyperbo.la>
2012-04-24 18:04:15 -0400

    Define multiple compute methods

--- jobs/wiki-wc.rb
+++ jobs/wiki-wc.rb
@@ -10,10 +10,11 @@ class WikipediaWordCount < Sr::Job::Jobfile
 
 
   def initialize
+    @ip = "localhost"
     @num_collectors = @num_fetchers = 1
-    @num_workers = 2
-    @num_revs = Sr::Util.send_message("air.local:7777", "num_revs", {})[:num_revs].to_i
-    # @num_revs = 500
+    @num_workers = 1
+    @num_revs = Sr::Util.send_message("#{@ip}:7777", "num_revs", {})[:num_revs].to_i
+    @num_revs = 500
     @seq = 0
     @results = Hash.new(0)
   end
@@ -29,12 +30,13 @@ class WikipediaWordCount < Sr::Job::Jobfile
 
   def fetcher_fetch_block(*args)
     return nil if @seq >= @num_revs
-    res = Sr::Util.send_message("air.local:7777", "next_rev/#{@seq}", {})
+    res = Sr::Util.send_message("#{@ip}:7777", "next_rev/#{@seq}", {})
     @seq += 1
     res["#{@seq-1}"]
   end
 
   def worker_init_block(obj)
+    # do the word count for real
     obj.add_compute_method(1.0) do |datum|
       result = Hash.new(0)
       datum = Zlib::Inflate.inflate(datum[:fulltext])
@@ -46,7 +48,47 @@ class WikipediaWordCount < Sr::Job::Jobfile
         word = word.gsub(/[\.,!\?]$/, "").downcase
        result[word] = result[word] + 1
       end
-      result
+      @cached_result = result
+    end
+    # do the word count for about half of the article
+    obj.add_compute_method(0.25) do |datum|
+      result = Hash.new(0)
+      datum = Zlib::Inflate.inflate(datum[:fulltext])
+      datum = Base64::decode64(datum)
+      # do the word count on a substring
+      start = datum.index(/\s/, rand(datum.length / 2))
+      stop = [datum.length - 1, start + datum.length / 2].min
+      len = stop - start
+      datum[start, len].split(/\s/).each do |word|
+        next if !(word =~ /^[\u0000-\u007F]+$/)
+        word = word.downcase
+        next if !(word =~ /^[a-z0-9\-]+$/)
+        word = word.gsub(/[\.,!\?]$/, "").downcase
+       result[word] = result[word] + 1
+      end
+      @cached_result = result
+    end
+    # do the word count for about a quarter of the article
+    obj.add_compute_method(0.25) do |datum|
+      result = Hash.new(0)
+      datum = Zlib::Inflate.inflate(datum[:fulltext])
+      datum = Base64::decode64(datum)
+      # do the word count on a substring
+      start = datum.index(/\s/, rand(datum.length / 2))
+      stop = datum.index(/\s/, start + rand(datum.length / 2))
+      len = stop - start
+      datum[start, len].split(/\s/).each do |word|
+        next if !(word =~ /^[\u0000-\u007F]+$/)
+        word = word.downcase
+        next if !(word =~ /^[a-z0-9\-]+$/)
+        word = word.gsub(/[\.,!\?]$/, "").downcase
+       result[word] = result[word] + 1
+      end
+      @cached_result = result
+    end
+    # return a hash for a previous article
+    obj.add_compute_method(0.1) do |datum|
+      @cached_result || Hash.new(0)
     end
   end
 end
